a3026996af30290c99f6341baa307a50de0f44f6
Barad Piyush <baradpiyush2528@gmail.com>
2025-06-03 13:37:20 +0530

    Add responsive navbar toggle and update styles

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,16 +1,30 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Style.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="navbar">
-      <Link to={'/'}>Home</Link>
-      <Link to={'/product'}>Product</Link>
-      <Link to={'/addproduct'}>Addproduct</Link>
-      <Link to={'/addcart'}>Add to Cart</Link>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-toggle" onClick={toggleNavbar}>
+        
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+        <Link to={'/'} onClick={() => setIsOpen(false)}>Home</Link>
+        <Link to={'/product'} onClick={() => setIsOpen(false)}>Product</Link>
+        <Link to={'/addproduct'} onClick={() => setIsOpen(false)}>Addproduct</Link>
+        <Link to={'/addcart'} onClick={() => setIsOpen(false)}>Add to Cart</Link>
+      </div>
+    </nav>
   );
 };
 
